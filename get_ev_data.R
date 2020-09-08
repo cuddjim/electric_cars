53dea1aad4d230a7cabff368e1539363109fa90a
cuddjim <jimmy.cuddihy@gmail.com>
2020-09-07 20:51:03 -0400

    tidy_table

--- get_ev_data.R
+++ get_ev_data.R
@@ -1,4 +1,4 @@
-library(rvest);library(tidyverse)
+library(rvest);library(tidyverse);library(formattable)
 
 # All data in this file are from the plugndrive.ca website
 #read url
@@ -26,7 +26,7 @@ elec_urls <- url %>% html_nodes('div.evCar a') %>% html_attr('href')
 the_car <- lapply(1:8, function(i) {map_chr(elec_urls, ~.x %>% 
                                                 read_html() %>% html_nodes('div.feature-container') %>% html_nodes('div.specs p') %>% .[i] %>% 
                                                 html_nodes('.value') %>% html_text())}) %>% 
-    setNames(c('category','drive_train','torque','passengers','cargo_space','weight','doors','motor'))
+    setNames(c('category','drive_train','torque_lb_ft','passengers','cargo_space_cubic_feet','weight_lbs','doors','motor'))
 
 ## extract performance info:
 # Horsepower
@@ -38,7 +38,7 @@ performance <- lapply(1:5, function(i) {map_chr(elec_urls, ~.x %>% read_html() %
                                                     html_nodes('div.other-features-container') %>% 
                                                     html_nodes('div.tab-content') %>% .[2] %>% 
                                                     html_nodes('.value') %>% html_text() %>% .[i])}) %>% 
-    setNames(c('horsepower','top_speed','e_range','zero_to_hunnid','t_range'))
+    setNames(c('horsepower','top_speed_km_per_h','e_range_km','zero_to_hunnid_s','t_range_km'))
 
 ## extract energy efficiency info:
 # City electric range (gas equivalent)
@@ -49,7 +49,7 @@ en_efficency <- lapply(1:4, function(i) {map_chr(elec_urls, ~.x %>% read_html() 
                                                      html_nodes('div.other-features-container') %>% 
                                                      html_nodes('div.tab-content') %>% .[3] %>% 
                                                      html_nodes('.value') %>% html_text() %>% .[i])}) %>% 
-    setNames(c('city_e','hwy_e','city_g','hwy_g'))
+    setNames(c('city_e_L100','hwy_e_L100','city_g_L100','hwy_g_L100'))
 
 ## extract battery info:
 # Size in kWh
@@ -59,21 +59,36 @@ battery <- lapply(1:3, function(i) {map_chr(elec_urls, ~.x %>% read_html() %>%
                                                 html_nodes('div.other-features-container') %>% 
                                                 html_nodes('div.tab-content') %>% .[4] %>% 
                                                 html_nodes('.value') %>% html_text() %>% .[i])}) %>% 
-    setNames(c('battery','charge_time','warranty'))
+    setNames(c('battery_kWh','charge_time_h_lvl2','warranty'))
 
 
 # extract emissions (CO2 kg)and cost ($) per 20,000 km driven for Ontario
-emissions_and_cost <- lapply(list('span.emissions','span.cost'), function(i) {map_chr(elec_urls, ~.x %>% read_html() %>% 
+emissions_and_cost_elec <- lapply(list('span.emissions','span.cost'), function(i) {map_chr(elec_urls, ~.x %>% read_html() %>% 
                                                                                           html_nodes('div.emissions-table') %>% 
                                                                                           html_nodes('div.table-body') %>% 
                                                                                           html_nodes('div.electric') %>% 
                                                                                           html_nodes(i) %>% html_text() %>% .[5])}) %>% 
-    setNames(c('emissions','cost'))
+    setNames(c('emissions_e_kg','cost_e_cnd'))
+
+emissions_and_cost_gas <- lapply(list('span.emissions','span.cost'), function(i) {map_chr(elec_urls, ~.x %>% read_html() %>% 
+                                                                                               html_nodes('div.emissions-table') %>% 
+                                                                                               html_nodes('div.table-body') %>% 
+                                                                                               html_nodes('div.gas') %>% 
+                                                                                               html_nodes(i) %>% html_text() %>% .[5])}) %>% 
+    setNames(c('emissions_g_kg','cost_g_cnd'))
 
 # Create table
-cars_table <- data.frame(model = car_list, price = prices, 
-                         lapply(list(the_car,performance,en_efficency,battery,emissions_and_cost),
-                                function (i) {data.frame(sapply(i,c))}) %>% bind_cols())
+cars_table <- data.frame(model = car_list, price_cnd = prices, 
+                         lapply(list(the_car,performance,en_efficency,battery,emissions_and_cost_elec,emissions_and_cost_gas),
+                                function (i) {data.frame(sapply(i,c))}) %>% bind_cols()) %>% select(-warranty) %>% 
+    mutate(charge_time_h_lvl2 = gsub('Level 2','',charge_time_h_lvl2)) %>% 
+    mutate_at(vars(matches('e_L100$')),~gsub('Litres Equivalent/100 km','',.)) %>% 
+    mutate_at(vars(matches('g_L100$')),~gsub('Litres/100 km','',.)) %>% 
+    mutate(torque_lb_ft = gsub('lb. ft.','',torque_lb_ft)) %>% 
+    mutate_at(vars(-c(model,category,drive_train)),~as.numeric(gsub("[^0-9.-]+", "",.))) %>% 
+    mutate(model = tools::toTitleCase(gsub('-',' ',model)),
+           model = gsub(' Phev','',model)) %>% 
+    mutate_at(vars(matches('L100$|lvl2$')),~comma(.,1))
+    # mutate_at(vars(matches('cnd$')),~scales::dollar(.,largest_with_cents = 1))
 
-
-
+write.csv(cars_table,'C:/Users/jimmy/OneDrive/Documents/repos/electric_cars/cars_table.csv', row.names = F)
